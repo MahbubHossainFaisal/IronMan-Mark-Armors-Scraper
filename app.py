20ce2c20f84c7c305c5b8a497b3ab58e4e1341ce
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2023-04-27 11:29:10 +0600

    basic structure of Iron man armor scrapper

--- app.py
+++ app.py
@@ -1,86 +1,30 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
-import requests
-from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen as uReq
-import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
-def homepage():
-    return render_template("index.html")
+# define armor data
+armor_data = {
+    'Mark I': {
+        'image': 'https://www.sideshow.com/storage/product-images/903253/mark-i-mark-i-prototype-iron-man_thumbnail.jpg',
+        'weapons': 'Flamethrower, missiles, machine guns',
+        'os': 'None',
+        'color': 'Silver',
+        'capabilities': 'Flight, superhuman strength, bulletproof',
+        'special_features': 'None'
+    }
+}
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        try:
-            searchString = request.form['content'].replace(" ","")
-            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-            uClient = uReq(flipkart_url)
-            flipkartPage = uClient.read()
-            uClient.close()
-            flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
-            box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+    # pass armor data to template
+    return render_template('index.html', armors=armor_data)
 
-            filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
-            reviews = []
-            for commentbox in commentboxes:
-                try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+@app.route('/armor/<armor_name>')
+def armor(armor_name):
+    # retrieve armor info based on armor name
+    armor_info = armor_data.get(armor_name)
+    # return armor info as JSON response
+    return armor_info
 
-                except:
-                    logging.info("name")
-
-                try:
-                    #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
-
-
-                except:
-                    rating = 'No Rating'
-                    logging.info("rating")
-
-                try:
-                    #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
-
-                except:
-                    commentHead = 'No Comment Heading'
-                    logging.info(commentHead)
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
-                except Exception as e:
-                    logging.info(e)
-
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
-                reviews.append(mydict)
-            logging.info("log my final result {}".format(reviews))
-            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
-        except Exception as e:
-            logging.info(e)
-            return 'something is wrong'
-    # return render_template('results.html')
-
-    else:
-        return render_template('index.html')
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
